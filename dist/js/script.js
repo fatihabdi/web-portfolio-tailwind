45c39a7d75df9c46419970208afc99af90460b7d
expressionless20 <dotaawtf23@gmail.com>
2022-07-25 18:58:45 +0700

    Update Dark Mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,35 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+window.addEventListener("click", function (e) {
+  if (e.target.id !== "hamburger" && e.target.id !== "nav-menu") {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Darkmode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
